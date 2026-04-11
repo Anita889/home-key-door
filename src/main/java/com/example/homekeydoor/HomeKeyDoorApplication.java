350995d932ee03d0886c624280cd6f3d82cd2ac8
Anita <anahit.mkrtchyan@puzl.ai>
2026-04-11 22:41:11 +0400

    - First commit.

---
+++ src/main/java/com/example/homekeydoor/HomeKeyDoorApplication.java
@@ -0,0 +1,13 @@
+package com.example.homekeydoor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeKeyDoorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeKeyDoorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homekeydoor/HomeKeyDoorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homekeydoor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeKeyDoorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
